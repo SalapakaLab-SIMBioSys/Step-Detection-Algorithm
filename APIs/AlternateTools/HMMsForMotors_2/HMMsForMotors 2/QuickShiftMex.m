dab433b16988837f27b64c11330c23d4156c36c8
Sivaraman <rajag038@umn.edu>
2020-04-22 04:45:56 -0500

    Release

--- APIs/AlternateTools/HMMsForMotors_2/HMMsForMotors 2/QuickShiftMex.m
+++
@@ -1,6 +0,0 @@
-function ms=QuickShiftMex(m,shift)
-% Faster replacement for circshift for time-critical applications.
-% About twice as fast as circshift.
-% Calls the mex routine QuickShiftMex, if available.
-%
-    ms=circshift(m,shift);    
--- APIs/AlternateTools/HMMsForMotors_2/HMMsForMotors 2/Step125.m
+++
@@ -1,8 +0,0 @@
-function xs=Step125(x)
-% Find the value in a 1..2..5 x 10^n sequence that is >= x.
-%
-n=floor(log10(x));
-x0=x/10^n;
-testvals=[0.5 1 2 5 10];
-index=find((x0<=testvals),1);
-xs=testvals(index)*10^n;
